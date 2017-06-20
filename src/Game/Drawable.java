1de4a968641241a64d2f1c381483d6f7b2c3e479
JulioZambrano <epczambrano@gmail.com>
2017-06-21 01:04:41 +0200

    GalaxyRider

---
+++ src/Game/Drawable.java
@@ -0,0 +1,15 @@
+
+package Game;
+
+import java.awt.Graphics;
+
+/**
+ *
+ * @JulioZambrano
+ * https://github.com/JulioZambrano/GalaxyRider
+ */
+public interface Drawable {
+    
+    public void draw(Graphics g);
+    
+}
---
+++ src/Game/Moveable.java
@@ -0,0 +1,12 @@
+
+package Game;
+/**
+ *
+ * @JulioZambrano
+ * https://github.com/JulioZambrano/GalaxyRider
+ */
+public interface Moveable {
+
+    public void move();
+
+}
